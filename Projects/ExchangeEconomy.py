38ff8a5a730cd2824e93c84e6b0a5327912be72b
zth948 <zth948@alumni.ku.dk>
2024-03-21 15:32:37 +0100

    Fed ændring

--- Projects/ExchangeEconomy.py
+++ Projects/ExchangeEconomy.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 import numpy as np
+from scipy import optimize
 
 class ExchangeEconomyClass:
 
@@ -28,13 +29,17 @@ class ExchangeEconomyClass:
     def demand_A(self,p1):
         par = self.par
         x1A = par.alpha*(p1*par.w1A+par.w2A)/p1
-        x2A = (1-par.alpha)*(p1*par.w1A+par.w2A) 
+        x2A = (1-par.alpha)*(p1*par.w1A+par.w2A)
+        x1A = max(0, min(1, x1A))
+        x2A = max(0, min(1, x2A))
         return x1A, x2A
 
     def demand_B(self,p1):
         par = self.par
         x1B = par.beta*(p1*par.w1B+par.w2B)/p1
         x2B = (1-par.beta)*(p1*par.w1B+par.w2B)
+        x1B = max(0, min(1, x1B))
+        x2B = max(0, min(1, x2B))
         return x1B, x2B 
     
     def initial_endowment(self):
@@ -43,76 +48,6 @@ class ExchangeEconomyClass:
         initial_utility_B = self.utility_B(par.w1B,par.w2B)
         return initial_utility_A, initial_utility_B
     
-    def pareto(self,N,do_print=True):
-        par = self.par
-
-        # a. Numpy array
-        shape_tuple = (N)
-        x1A_values = np.empty(shape_tuple)
-        x2A_values = np.empty(shape_tuple)
-        x1B_values = np.empty(shape_tuple)
-        x2B_values = np.empty(shape_tuple)
-        uA_values = np.empty(shape_tuple)
-        uB_values = np.empty(shape_tuple)
-
-        # b. start from guess
-        x1A_initial = 0.8
-        x2A_initial = 0.3
-        x1B_initial = 0.2
-        x2B_initial = 0.7
-        # we have already expressions for endowmnet values and utility
-
-        # c. loop through possibilities
-        for i in range(N):
-
-            # i. x1
-            x1A_values[i] = x1A = i/N
-            x1B_values[i] = x1B = 1 - x1A_values[i]
-
-            # ii. x2
-            x2A_values[i] = x2A = i/N
-            x2B_values[i] = x2B = 1 - x2A_values[i]
-
-            # utilities 
-            uA_values[i] = uA = self.utility_A(x1A,x2A)
-            uB_values[i] = uB = self.utility_B(x1B,x2B)
-
-            if uA_values[i] >= self.utility_A(par.w1A,par.w2A):
-                x1A_initial = x1A_values[i]
-                x2A_initial = x2A_values[i]
-                self.initial_utility_A = uA_values[i]
-
-            if uB_values[i] >= self.utility_B(par.w1B,par.w2B):
-                x1B_initial = x1B_values[i]
-                x2B_initial = x2B_values[i]
-                self.initial_utility_B = uB_values[i]
-
-        # d. print
-        #if do_print:
-         #   print_solution(x1A_initial,x2A_initial,x1B_initial,x2B_initial,uA_values,uB_values)
-
-        return x1A_initial,x2A_initial,x1B_initial,x2B_initial,uA_values,uB_values,uA,uB
-    
-    # function for printing the solution
-#def print_solution(x1A,x2A,x1B,x2B,uA,uB):
-    #print(f'x1A = {x1A:.2f}')
-    #print(f'x2A = {x2A:.2f}')
-    #print(f'x1B = {x1B:.2f}')
-    #print(f'x2B = {x2B:.2f}')
-    #print(f'uA  = {uA:.2f}')
-    #print(f'uB  = {uB:.2f}')
-
-
-
-
-
-
-
-
-
-
-
-
 
     # For question 2
     def check_market_clearing(self,p1):
@@ -126,3 +61,48 @@ class ExchangeEconomyClass:
         eps2 = x2A-par.w2A + x2B-(1-par.w2A)
 
         return eps1,eps2
+
+    # For question 4a
+    def optimal_allocation(self):
+
+        # create empty 
+        max_utility = float('-inf')
+        optimal_price = None
+        optimal_allocation = (None, None)
+
+        # define the prices
+        prices = np.linspace(0.5,2.5,75)
+
+        # deceide B's consumption
+        for p1 in prices:
+            x1_B_star,x2_B_star = self.demand_B(p1)
+
+            # allocate rest to A
+            remaining_x1A = 1- x1_B_star
+            remaining_x2A = 1- x2_B_star
+
+            # ccalculate A's utility 
+            if remaining_x1A >= 0 and remaining_x2A >= 0:
+                utility_for_A = self.utility_A(remaining_x1A,remaining_x2A)
+            else:
+                utility_for_A = 0
+
+            # check if this is greater
+            if utility_for_A > max_utility:
+                max_utility = utility_for_A
+                optimal_price = p1
+                optimal_allocation = (remaining_x1A, remaining_x2A)
+
+        return max_utility, optimal_price, optimal_allocation
+    
+
+
+
+
+
+    
+
+
+
+        
+
